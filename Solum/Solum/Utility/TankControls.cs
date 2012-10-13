b40a941ff5329cfb5cee8912c1f0cfba5298be00
teemu <teemu@rabbit-PC>
2012-10-13 15:29:13 +0300

    Lisää tankkia jeesukselle.

---
+++ Solum/Solum/Utility/TankControls.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Solum.Utility;
+using Solum.Input;
+using Solum.Logging;
+
+namespace Solum.Utility
+{
+    public enum ControlSide
+    {
+        Right,
+        Left
+    }
+
+    class TankControls
+    {
+        public ControlSide darkside;
+        public Buttons shoot;
+        public Buttons changeWeapon;
+        public Buttons turretRotateCW;
+        public Buttons turretRotateCCW;
+
+        public TankControls(ControlSide side)
+        {
+            darkside = side;
+
+            if (darkside == ControlSide.Left)
+            {
+                turretRotateCW = Buttons.DPadLeft;
+                turretRotateCCW = Buttons.DPadRight;
+                shoot = Buttons.LeftShoulder;
+                changeWeapon = Buttons.LeftTrigger;
+            }
+            else
+            {
+                turretRotateCW = Buttons.B;
+                turretRotateCCW = Buttons.X;
+                shoot = Buttons.RightShoulder;
+                changeWeapon = Buttons.RightTrigger;
+            }
+        }
+    }
+}
